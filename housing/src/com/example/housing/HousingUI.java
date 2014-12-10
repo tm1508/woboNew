94e939130e0cb198d1dd563d249ae341cab6fcf4
Julia <Julia@172.16.56.19>
2014-12-10 16:19:34 +0100

    Test

--- housing/src/com/example/housing/HousingUI.java
+++ housing/src/com/example/housing/HousingUI.java
@@ -1,8 +1,5 @@
 package com.example.housing;
 
-import java.util.Spliterator;
-import java.util.function.Consumer;
-
 import javax.servlet.annotation.WebServlet;
 
 import com.vaadin.annotations.Theme;
@@ -10,13 +7,6 @@ import com.vaadin.annotations.VaadinServletConfiguration;
 import com.vaadin.navigator.Navigator;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.server.VaadinServlet;
-import com.vaadin.ui.Button.ClickEvent;
-import com.vaadin.ui.CheckBox;
-import com.vaadin.ui.Component;
-import com.vaadin.ui.HorizontalLayout;
-import com.vaadin.ui.Label;
-import com.vaadin.ui.PopupDateField;
-import com.vaadin.ui.TextField;
 import com.vaadin.ui.UI;
 import com.vaadin.ui.VerticalLayout;
 
@@ -42,19 +32,6 @@ public class HousingUI extends UI {
 		navigator.addView(name, new Startseite());
 		navigator.navigateTo(name);
 	}
-
-	@Override
-	public void forEach(Consumer<? super Component> arg0) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public Spliterator<Component> spliterator() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-	
 
 
 }
