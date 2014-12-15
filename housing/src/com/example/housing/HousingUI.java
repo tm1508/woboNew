beb880724ad338f3b101582bea50e1fc6cba7c6d
Julia <Julia@172.16.56.19>
2014-12-15 12:50:45 +0100

    Login Fenster und Session erzeugen

--- housing/src/com/example/housing/HousingUI.java
+++ housing/src/com/example/housing/HousingUI.java
@@ -12,11 +12,19 @@ import javax.persistence.Query;
 import javax.persistence.SynchronizationType;
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.metamodel.Metamodel;
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
+
+
 
 import com.vaadin.annotations.Theme;
 import com.vaadin.annotations.VaadinServletConfiguration;
 import com.vaadin.navigator.Navigator;
+import com.vaadin.server.ServiceException;
+import com.vaadin.server.SessionDestroyEvent;
+import com.vaadin.server.SessionDestroyListener;
+import com.vaadin.server.SessionInitEvent;
+import com.vaadin.server.SessionInitListener;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.server.VaadinServlet;
 import com.vaadin.ui.UI;
@@ -30,7 +38,27 @@ public class HousingUI extends UI {
 	
 	@WebServlet(value = "/*", asyncSupported = true)
 	@VaadinServletConfiguration(productionMode = false, ui = HousingUI.class)
-	public static class Servlet extends VaadinServlet {
+	public static class Servlet extends VaadinServlet implements SessionInitListener, SessionDestroyListener{
+
+			@Override
+			protected void servletInitialized() throws ServletException {
+			super.servletInitialized();
+			getService().addSessionInitListener(this);
+			getService().addSessionDestroyListener(this);
+			}
+			@Override
+			public void sessionInit(SessionInitEvent event)
+			throws ServiceException {
+			
+				event.getSession().setAttribute("login", false);
+				event.getSession().setAttribute("user", null);
+				event.getSession().setAttribute("dhstud", false);
+				
+			}
+			@Override
+			public void sessionDestroy(SessionDestroyEvent event) {
+			
+			}
 	}
 
 	@Override
