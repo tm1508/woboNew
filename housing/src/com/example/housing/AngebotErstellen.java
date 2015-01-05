173d399ccccc74f8dcc41d5929be2644b22d4837
Sarah- <SaJean1993@aol.com>
2015-01-06 00:36:08 +0100

    NullPointerExceptions abfangen und andere Überprüfungen hinzugefügt

--- housing/src/com/example/housing/AngebotErstellen.java
+++ housing/src/com/example/housing/AngebotErstellen.java
@@ -2,6 +2,7 @@ package com.example.housing;
 
 import com.example.housing.data.model.Offer;
 import com.example.housing.data.provider.OfferProvider;
+import com.vaadin.data.Validator.InvalidValueException;
 import com.vaadin.navigator.View;
 import com.vaadin.navigator.ViewChangeListener.ViewChangeEvent;
 import com.vaadin.server.VaadinSession;
@@ -11,6 +12,7 @@ import com.vaadin.ui.ComboBox;
 import com.vaadin.ui.DateField;
 import com.vaadin.ui.HorizontalLayout;
 import com.vaadin.ui.Label;
+import com.vaadin.ui.Notification;
 import com.vaadin.ui.RichTextArea;
 import com.vaadin.ui.TextField;
 import com.vaadin.ui.Upload;
@@ -81,6 +83,8 @@ public class AngebotErstellen extends VerticalLayout implements View {
 		ltitel.setWidth("10%");
 		ltitel.addStyleName("AbschnittLabel");
 		final TextField titel = new TextField();
+		titel.setRequired(true);
+		titel.setRequiredError("Bitte geben Sie einen Titel an.");
 		titel.setWidth("90%");
 		titel.addStyleName("ImportantTitle");
 		hl.addComponent(ltitel);
@@ -88,13 +92,19 @@ public class AngebotErstellen extends VerticalLayout implements View {
 		Label adress = new Label("Adresse");
 		adress.addStyleName("AbschnittLabel");
 		final TextField street = new TextField("Straße, Hausnummer");
+		street.setRequired(true);
+		street.setRequiredError("Bitte geben Sie Straße und Hausnummer an.");
 		street.addStyleName("AngeboteTextField");
 		HorizontalLayout hl0 = new HorizontalLayout();
 		hl0.setWidth("50%");
 		final TextField zip = new TextField("PLZ");
+		zip.setRequired(true);
+		zip.setRequiredError("Bitte geben Sie die Postleitzahl an.");
 		zip.setWidth("50%");
 		zip.addStyleName("AngeboteTextField");
 		final TextField city = new TextField("Ort");
+		city.setRequired(true);
+		city.setRequiredError("Bitte geben Sie den Ort an.");
 		city.addStyleName("AngeboteTextField");
 		city.setWidth("50%");
 		hl0.addComponent(zip);
@@ -121,21 +131,28 @@ public class AngebotErstellen extends VerticalLayout implements View {
 		allgInfo.addStyleName("AbschnittLabel");
 		label.addComponent(allgInfo);
 		final ComboBox isShared = new ComboBox("Art");
-		isShared.setNullSelectionItemId("Wohnung");
+		isShared.setRequired(true);
+		isShared.setRequiredError("Bitte geben sie die Art des Angebots an.");
 		isShared.addItem("Wohnung");
 		isShared.addItem("Zimmer");
 		isShared.addItem("WG-Zimmer");
 		isShared.addStyleName("AngeboteTextField");
 		z1.addComponent(isShared);
 		final TextField squareMetre = new TextField("Größe (in m²)");
+		squareMetre.setRequired(true);
+		squareMetre.setRequiredError("Bitte geben Sie die Größe in m² an.");
 		squareMetre.addStyleName("AngeboteTextField");
 		z2.addComponent(squareMetre);
 		final TextField roomMates = new TextField("Anzahl Mitbewohner:");
+		roomMates.setRequired(true);
+		roomMates.setRequiredError("Bitte geben Sie die Anzahl an Mitbewohner an.");
 		roomMates.setValue("0");
 		roomMates.addStyleName("AngeboteTextField");
 		z3.addComponent(roomMates);
 		Label date = new Label("Verfügbarkeit");
 		final DateField startDate = new DateField("von:");
+		startDate.setRequired(true);
+		startDate.setRequiredError("Bitte geben Sie ein Startdatum an.");
 		final DateField endDate = new DateField("bis:");
 		HorizontalLayout hl1 = new HorizontalLayout();
 		startDate.addStyleName("AngeboteTextField");
@@ -149,6 +166,8 @@ public class AngebotErstellen extends VerticalLayout implements View {
 		costs.addStyleName("AbschnittLabel");
 		label.addComponent(costs);
 		final TextField price = new TextField("Warmmiete:");
+		price.setRequired(true);
+		price.setRequiredError("Bitte geben Sie die Warmmiete an.");
 		price.addStyleName("AngeboteTextField");
 		z1.addComponent(price);
 		final TextField bond = new TextField("Kaution:");
@@ -190,7 +209,8 @@ public class AngebotErstellen extends VerticalLayout implements View {
 		pets.setWidth("20%");
 		pets.addStyleName("AngeboteTextField");
 		final ComboBox genders = new ComboBox("Bevorzugtes Geschlecht:");
-		genders.setNullSelectionItemId("egal");
+		genders.setRequired(true);
+		genders.setRequiredError("Bitte geben Sie ein ob es ein bevorzugtes Geschlecht gibt.");
 		genders.addItem("egal");
 		genders.addItem("männlich");
 		genders.addItem("weiblich");
@@ -212,6 +232,8 @@ public class AngebotErstellen extends VerticalLayout implements View {
 		Label anzeigetext = new Label("Anzeigetext");
 		anzeigetext.addStyleName("AbschnittLabel");
 		final RichTextArea text = new RichTextArea();
+		text.setRequired(true);
+		text.setRequiredError("Bitte geben Sie eine kurze Beschreibun des Angebots an.");
 		text.setWidth("100%");
 		Label bilder = new Label("Bilder hinzufügen");
 		bilder.addStyleName("AbschnittLabel");
@@ -229,61 +251,138 @@ public class AngebotErstellen extends VerticalLayout implements View {
 
 		// Button speichern/aktivieren/deaktivieren
 		final CheckBox inactive = new CheckBox("deaktivieren");
-		
+
 		Button save = new Button("speichern");
 		save.addStyleName("BearbeitenButton");
 		save.addClickListener(new Button.ClickListener() {
 			public void buttonClick(ClickEvent event) {
+				// Überprüfung ob alle Mussfelder gefüllt sind
+				boolean valid = true; // fals ein Mussfeld nicht gefüllt ist
+										// wird valid = false gesetzt
+				try {
+					titel.validate();
+				} catch (InvalidValueException e) {
+					valid = false;
+				}
+				try {
+					street.validate();
+				} catch (InvalidValueException e) {
+					valid = false;
+				}
+				try {
+					zip.validate();
+				} catch (InvalidValueException e) {
+					valid = false;
+				}
+				try {
+					city.validate();
+				} catch (InvalidValueException e) {
+					valid = false;
+				}
+				try {
+					startDate.validate();
+				} catch (InvalidValueException e) {
+					valid = false;
+				}
+				try {
+					genders.validate();
+				} catch (InvalidValueException e) {
+					valid = false;}
+				try {
+					isShared.validate();
+				} catch (InvalidValueException e) {
+					valid = false;}
+				try {
+					squareMetre.validate();
+				} catch (InvalidValueException e) {
+					valid = false;
+				}
+				try {
+					price.validate();
+				} catch (InvalidValueException e) {
+					valid = false;
+				}
+				try {
+					roomMates.validate();
+				} catch (InvalidValueException e) {
+					valid = false;
+				}
+				try {
+					bond.validate();
+				} catch (InvalidValueException e) {
+					valid = false;
+				}
+				try {
+					text.validate();
+				} catch (InvalidValueException e) {
+					valid = false;
+				}
+
+				// Überprüft welche Art Angebot erstellt wird und setzt entsprechenden int-Wert
 				int type = 1;
-				if (isShared.getValue().equals("Wohnung")) {
+				String shared = (String) isShared.getValue();
+				if (shared.equals("Wohnung")) {
 					type = 1;
-				} else if (isShared.getValue().equals("Zimmer")) {
+				} else if (shared.equals("Zimmer")) {
 					type = 2;
-				} else if (isShared.getValue().equals("WG-Zimmer")) {
+				} else if (shared.equals("WG-Zimmer")) {
 					type = 3;
 				}
+				
+				// Überprüft ob es ein bevorzugtes Geschlecht gibt und setzt entsprechenden int-Wert
 				int gender = 1;
-				if (genders.getValue().equals("männlich")) {
+				String gend = (String) genders.getValue();
+				if(gend.equals("egal")) {
+					gender = 1;
+				}else if(gend.equals("männlich")) {
 					gender = 2;
-				} else if (genders.getValue().equals("weiblich")) {
+				} else if (gend.equals("weiblich")) {
 					gender = 3;
 				}
+
 				OfferProvider of = new OfferProvider();
 				Offer newOffer = new Offer();
-				
-				newOffer.setTitle(titel.getValue());
-				newOffer.setStreet(street.getValue());
-				newOffer.setZip(zip.getValue());
-				newOffer.setCity(city.getValue());
-				newOffer.setStartDate(startDate.getValue());
-				newOffer.setEndDate(endDate.getValue());
-				newOffer.setSquareMetre(Float.parseFloat(squareMetre.getValue()));
-				newOffer.setPrice(Float.parseFloat(price.getValue()));
-				newOffer.setType(type);
-				newOffer.setNumberOfRoommate(Integer.parseInt(roomMates.getValue()));
-				newOffer.setInternet(internet.getValue());
-				newOffer.setFurnished(furnished.getValue());
-				newOffer.setKitchen(kitchen.getValue());
-				newOffer.setSmoker(smoker.getValue());
-				newOffer.setPets(pets.getValue());
-				newOffer.setGender(gender);
-				newOffer.setText(text.getValue());
-				newOffer.setBond(Float.parseFloat(bond.getValue()));
-				newOffer.setInactive(inactive.getValue());
-				//newOffer.setLatitude(latitude);
-				//newOffer.setLongitude(longitude);
-				//newOffer.setPhotos();
-				of.addOffer(newOffer);
-				String name = "AngebotAnzeigen";
-				getUI().getNavigator().addView(name, new AngebotAnzeigen(newOffer.getIdOffer()));
-				getUI().getNavigator().navigateTo(name);
-				
+				if (valid) {// sind alle Mussfelder gefüllt, wird ein neues
+							// Angebot erstellt
+					newOffer.setTitle(titel.getValue());
+					newOffer.setStreet(street.getValue());
+					newOffer.setZip(zip.getValue());
+					newOffer.setCity(city.getValue());
+					newOffer.setStartDate(startDate.getValue());
+					try{
+						newOffer.setEndDate(endDate.getValue());}
+					catch(NullPointerException e){}
+					newOffer.setSquareMetre(Float.parseFloat(squareMetre.getValue()));
+					newOffer.setPrice(Float.parseFloat(price.getValue()));
+					newOffer.setType(type);
+					newOffer.setNumberOfRoommate(Integer.parseInt(roomMates.getValue()));
+					newOffer.setInternet(internet.getValue());
+					newOffer.setFurnished(furnished.getValue());
+					newOffer.setKitchen(kitchen.getValue());
+					newOffer.setSmoker(smoker.getValue());
+					newOffer.setPets(pets.getValue());
+					newOffer.setGender(gender);
+					newOffer.setText(text.getValue());
+					if (bond.getValue().equals(""))
+						newOffer.setBond(Float.parseFloat("0.0"));
+					else
+						newOffer.setBond(Float.parseFloat(bond.getValue()));
+					newOffer.setInactive(inactive.getValue());
+					// newOffer.setLatitude(latitude);
+					// newOffer.setLongitude(longitude);
+					// newOffer.setPhotos();
+					of.addOffer(newOffer);
+					String name = "AngebotAnzeigen";
+					getUI().getNavigator().addView(name, new AngebotAnzeigen(newOffer.getIdOffer()));
+					getUI().getNavigator().navigateTo(name);
+
+				} else
+					Notification.show("Bitte füllen Sie alle Mussfelder*");
 			}
 		});
 
 		content.addComponent(inactive);
 		content.addComponent(save);
-		
 
 	}
 
