599df41fdee003d365138ab997a28798b5edae00
Sarah- <SaJean1993@aol.com>
2015-01-07 15:52:49 +0100

    Kommentare gelöscht

--- housing/src/com/example/housing/Suchergebnis.java
+++ housing/src/com/example/housing/Suchergebnis.java
@@ -93,95 +93,15 @@ public class Suchergebnis extends VerticalLayout implements View {
 	 */
 	@SuppressWarnings("deprecation")
 	public void setContent(){
-		
-		
+			
 		// Anzahl der gefundenen Ergebnisse
 		int anzahl = angebote.size();
 		System.out.println(anzahl);
 		for(int i = 0; i<anzahl;i++){
 			final Offer o = angebote.get(i);
 		
-	
-
-		/*	GridLayout ergebnisLayout = new GridLayout(6,3);
-			ergebnisLayout.setMargin(false);
-			//pictures
-			List<Photo> pictures;
-			pictures = o.getPhotos();
-			ImageInputStream iis = null;
-			//falls Bilder zu der Wohnung vorhanden sind
-			if(pictures.size()>0){
-			Photo ph = pictures.get(0);			
-			byte[] by = ph.getPhoto();
-			ByteArrayInputStream bis = new ByteArrayInputStream(by);
-			Object source = bis;
-			try {
-				iis = ImageIO.createImageInputStream(source);
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-			Buffered bufferedImage=null;
-			try {
-				bufferedImage = (Buffered) ImageIO.read(iis);
-			} catch (IOException e) {
-				e.printStackTrace();
-			} 
-			Image ii = (Image) bufferedImage;	
-			ergebnisLayout.addComponent(ii,0,0,2,2);
-			}else{
-			//TODO Kein bild vorhanden Bild
-			String basepath = VaadinService.getCurrent().getBaseDirectory().getAbsolutePath();
-			FileResource resource = new FileResource(new File(basepath + "/WEB-INF/image/dh.jpg"));
-			Image image = new Image("",resource);
-			ergebnisLayout.addComponent(image,0,0,2,2);
-//			String im = "Kein Bild vorhanden";
-//			ergebnisLayout.addComponent(new Label(im),0,0,2,2);
-			}
-			
-			String title = o.getTitle();
-			Label l = new Label(title + " in "+ o.getCity());
-			ergebnisLayout.addComponent(l,4,0);
-			float price = o.getPrice();		
-			Label l2 = new Label(Float.toString(price)+ " €");
-			ergebnisLayout.addComponent(l2,3,1);
-			
-			float sm = o.getSquareMetre();
-			String sSm = Float.toString(sm) + " m²";
-			ergebnisLayout.addComponent(new Label(sSm),4,1);
-			
-			int a = o.getType();
-			String s;
-			if(a ==2){
-				s = "WG";
-			}else if(a ==1){
-				s = "Wohnung";
-			}else{
-				s="";
-			}
-			ergebnisLayout.addComponent(new Label(s),5,1);
-			
-			
-//			Date start= new Date(2015,01,30);
-			Date start = (Date) o.getStartDate();
-			ergebnisLayout.addComponent(new Label(start.toString()),3,2);
-//			Date end= new Date(2015,04,30);
-			//TODO: Wenn end-Datum nicht gesetzt ist, prüfen!
-			Date end = (Date) o.getEndDate();
-			ergebnisLayout.addComponent(new Label(end.toString()),5,2);
-			
-			
-			ergebnisLayout.addLayoutClickListener(new LayoutClickListener(){
-				public void layoutClick(LayoutClickEvent event) {
-					String name = "Einzelansicht";
-					getUI().getNavigator().addView(name, new Einzelansicht(o));
-					getUI().getNavigator().navigateTo(name);
-	            }
-			});
-		*/
 			content.addComponent(new Listenzeile(o));
-			//content.addComponent(ergebnisLayout);
 		}
-
 
 	}
 
