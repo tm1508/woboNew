2007bd15c2f4d80861e8e50f786078934a21254f
Sarah- <SaJean1993@aol.com>
2015-01-15 10:05:12 +0100

    FAQ erweitert

--- housing/src/com/example/housing/FAQ.java
+++ housing/src/com/example/housing/FAQ.java
@@ -112,10 +112,10 @@ public class FAQ extends VerticalLayout implements View{
 			layout.addComponent(sub_accordion);
 	        accordion.addTab(layout, "zu Ihrem Accout",FontAwesome.THUMB_TACK);//Tab 1 hinzufügen
          
-	        //Tab 2 zu Wohnungsangebote
+	        //Tab 2 zu Wohnungsangebote als Anbieter
 			final VerticalLayout layout_1 = new VerticalLayout();
 	        layout_1.setMargin(true);
-	        //Unterkategorien zu Fragen zu Wohnungsangebote
+	        //Unterkategorien zu Fragen zu Wohnungsangebote als Anbieter
 	        Accordion sub_accordion_1 = new Accordion();
 			sub_accordion_1.setStyleName("startseite");
 			
@@ -128,52 +128,38 @@ public class FAQ extends VerticalLayout implements View{
 			final Label label_5 = new Label("Um ein Angebot löschen zu können, müssen Sie sich zunächst einloggen. \nKlicken Sie anschließend auf die Registerkarte Wohnung einstellen und Wohnungsangebote verwalten. \nNun erscheint eine Liste Ihrer Angebote. Wählen Sie das gewünschte Angebot aus. \nAm Ende der Seite befindet sich ein Button \"Löschen\". Hinweis: Haben Sie vor das Angebot ggf. später wieder einzustellen ist es möglich, das Angebot für einen Zeitraum zu deaktivieren.");
 			sub_accordion_1.addTab(label_5, "Wie kann ich ein Wohnungsangebot löschen?", FontAwesome.QUESTION);
 			
-			final Label label_6 = new Label("Um ein Angebot bearbeiten zu können, müssen Sie sich zunächst einloggen. \nKlicken Sie anschließend auf die Registerkarte Wohnung einstellen und Wohnungsangebote verwalten. \nNun erscheint eine Liste Ihrer Angebote. Wählen Sie das gewünschte Angebot aus. \nKlicken Sie den Button \"Bearbeiten\". Am Ende der Seite befindet sich ein Kästchen um das Angebot zu deaktivieren.");
+			final Label label_6 = new Label("Um ein Angebot deaktivieren zu können, müssen Sie sich zunächst einloggen. \nKlicken Sie anschließend auf die Registerkarte Wohnung einstellen und Wohnungsangebote verwalten. \nNun erscheint eine Liste Ihrer Angebote. Wählen Sie das gewünschte Angebot aus. \nKlicken Sie den Button \"Bearbeiten\". Am Ende der Seite befindet sich ein Kästchen um das Angebot zu deaktivieren.");
 			sub_accordion_1.addTab(label_6, "Wie kann ich ein Wohnungsangebot deaktivieren?", FontAwesome.QUESTION);
-		        
+
+			
 			layout_1.addComponent(sub_accordion_1);
-			accordion.addTab(layout_1, "zu Wohnungsangebote",FontAwesome.THUMB_TACK);//Tab 2 hinzufügen
-        
-	        //Tab 3
+			accordion.addTab(layout_1, "zu Wohnungsangebote als Anbieter",FontAwesome.THUMB_TACK);//Tab 2 hinzufügen
+
+			//Tab 3   zu Wohnungsangebote
 			final VerticalLayout layout_2 = new VerticalLayout();
 	        layout_2.setMargin(true);
-	        	layout_2.addComponent(new Label("Für alle, die eine Wohnung suchen...: Sie sind Dualer Student an der DHBW Karlsruhe? Dann finden Sie bei uns die passende Wohung oder das passende Zimmer. Wir haben ein großes Angebot an Zimmern und Wohnungen..."));
-	
-				//Link zu Registrierung
-				Button link_2 = new Button();
-				link_2.setStyleName("link");
-				link_2.setCaption("Registrierung");
-				link_2.setImmediate(false);
-				link_2.setWidth("-1px");
-				link_2.setHeight("-1px");
-				link_2.setIcon(FontAwesome.EXTERNAL_LINK);
-				link_2.addClickListener(new Button.ClickListener(){
-					public void buttonClick(ClickEvent event) {
-						String name = "Registrierung";
-						getUI().getNavigator().addView(name, new Registrierung());
-						getUI().getNavigator().navigateTo(name);
-					}
-				});
-				
-				layout_2.addComponent(link_2);
-				
-			accordion.addTab(layout_2, "Für Mieter...",FontAwesome.SEARCH);//Tab 3 hinzufügen
+			//Unterkategorien zu Fragen zu Wohnungsangebote	     			
+			Accordion sub_accordion_2 = new Accordion();
+			sub_accordion_2.setStyleName("startseite");
+	        
+	        final Label label_8 = new Label("Ist ein Angebot nicht mehr aktuell und der Anbieter löscht dieses Angebot, wird es automatisch aus allen Listen gelöscht.");
+			sub_accordion_2.addTab(label_8, "Warum ist ein Wohnungsangebot in meinen Favoriten verschwunden?", FontAwesome.QUESTION);
+			
+			final Label label_9 = new Label("Um ein Anfrage senden zu können, müssen Sie sich zunächst als DH-Student verifizieren. \nKlicken Sie anschließend auf das gewünschte Angebot. Am Ende der Seite befindet sich ein Button \"Anfragen\", drücken Sie diesen. \nNun kommen Sie zum Anfrageformular. In dem Textfeld können Sie nun Ihre Anfrage formulieren und mit einem Klick auf den Button \"Anfrage versenden\" wird die Anfrage an den Anbieter weitergeleitet.");
+			sub_accordion_2.addTab(label_9, "Wie kann ich eine Anfrage für ein Wohnungsangebot senden?", FontAwesome.QUESTION);
+			
+			final Label label_10 = new Label("Hat der Anbieter eines Angebots genug Anfragen oder steht das Angebot aus anderen Gründen zur Zeit nicht zur Verfügung, \nhat der Anbieter die Möglichkeit das Angebot zu deaktivieren und zu einem späteren Zeitpunkt wieder zu aktivieren. \nJedoch kann während ein Angebot deaktiviert ist keine Anfrage gestellt werden.");
+			sub_accordion_2.addTab(label_10, "Warum kann ich zu einem Wohnungsangebot keine Anfrage senden?", FontAwesome.QUESTION);
+			//TODO
+			//Warum wird meine Anfrage nicht verschickt
+			layout_2.addComponent(sub_accordion_2);
+			accordion.addTab(layout_2, "zu Wohnungsangebote allgemein",FontAwesome.THUMB_TACK);//Tab 3 hinzufügen
         
 	        //Tab 4
 			final VerticalLayout layout_3 = new VerticalLayout();
 	        layout_3.setMargin(true);
-		        layout_3.addComponent(label_3);
-		        layout_3.addComponent(label_4);
-		        
-		        Link link_3 = new Link();
-				link_3.setStyleName("text");
-				link_3.setCaption("Kontakt");
-				link_3.setImmediate(false);
-				link_3.setWidth("-1px");
-				link_3.setHeight("-1px");
-				link_3.setIcon(FontAwesome.EXTERNAL_LINK);
-				layout_3.addComponent(link_3);
-	        accordion.addTab(layout_3, "Über uns...",FontAwesome.ENVELOPE);//Tab 4 hinzufügen
+		       
+	        accordion.addTab(layout_3, "Über uns...",FontAwesome.THUMB_TACK);//Tab 4 hinzufügen
          
 		content.addComponent(accordion);//verschiedene Kategorien hinzufügen
 		
