543b31e34ab3af8a9cca4cbc106b8a5d28d52f10
Julia <Julia@Julia-PC>
2015-05-08 18:13:47 +0200

    Refactoring Error Page

--- housing/src/com/example/housing/ErrorPage.java
+++ housing/src/com/example/housing/ErrorPage.java
@@ -10,7 +10,6 @@ import com.vaadin.ui.Label;
 import com.vaadin.ui.Panel;
 import com.vaadin.ui.VerticalLayout;
 
-// TODO: Auto-generated Javadoc
 /**
  * The Class Error Page.
  * 
@@ -19,98 +18,99 @@ import com.vaadin.ui.VerticalLayout;
  * @see com.example.housing.HousingUI
  */
 @SuppressWarnings("serial")
-public class ErrorPage extends HorizontalLayout implements View{
-	
-	private com.vaadin.server.ErrorEvent event;
-	
+public class ErrorPage extends HorizontalLayout implements View {
+
 	/** The content. */
-	private VerticalLayout content;//Layout fuer den Inhalt
-	
+	private VerticalLayout content;// Layout fuer den Inhalt
+
 	/** The title. */
-	private Label title;
-	
+	private Label titleLabel;
+
 	/** The text. */
-	private Label text;
-	
-	/* (non-Javadoc)
-	 * @see com.vaadin.navigator.View#enter(com.vaadin.navigator.ViewChangeListener.ViewChangeEvent)
+	private Label errorTextLabel;
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * com.vaadin.navigator.View#enter(com.vaadin.navigator.ViewChangeListener
+	 * .ViewChangeEvent)
 	 */
 	@Override
 	public void enter(ViewChangeEvent event) {
-		
+
 	}
-	
+
 	/**
 	 * Instantiates a new ErrorPage.
 	 */
-	public ErrorPage(){
-		
-		this.event = event;
-		
+	public ErrorPage() {
+
 		this.setWidth("100%");
-		
-		//linkes rotes Panel
+
+		// linkes rotes Panel
 		Panel p = new Panel();
 		p.setWidth("100%");
 		p.setHeight("100%");
 		p.addStyleName("red");
 		addComponent(p);
 		this.setExpandRatio(p, 1);
-		
-		//mittlerer Teil der Seite
+
+		// mittlerer Teil der Seite
 		VerticalLayout v = new VerticalLayout();
-				
-			//Navigation hinzufuegen
-			Navigation nav = new Navigation();
-			nav.setWidth("100%");
-			nav.addStyleName("navigation");
-			v.addComponent(nav);
-			
-			NavigationPublic navPublic = new NavigationPublic();
-			v.addComponent(navPublic);
-			
-			NavigationAdmin navAdmin = new NavigationAdmin();
-			v.addComponent(navAdmin);
-			
-			//falls der Benutzer eingelogt ist verändert sich die Navigation
-			if(VaadinSession.getCurrent().getAttribute("login").equals(true)){
-				if(VaadinSession.getCurrent().getAttribute(User.class).getAccessLevel()==2){//falls der User ein Admin ist
-					nav.setVisible(false);
-					navPublic.setVisible(false);
-					navAdmin.setVisible(true);//Admin-Navigation
-				}else{//ansonsten: Naviagtion für eingeloggte Nutzer
-					nav.setVisible(true);
-					navPublic.setVisible(false);
-					navAdmin.setVisible(false);
-				}
-			}else{//ansonsten Public Navigation (für alle)
+
+		// Navigation hinzufuegen
+		Navigation nav = new Navigation();
+		nav.setWidth("100%");
+		nav.addStyleName("navigation");
+		v.addComponent(nav);
+
+		NavigationPublic navPublic = new NavigationPublic();
+		v.addComponent(navPublic);
+
+		NavigationAdmin navAdmin = new NavigationAdmin();
+		v.addComponent(navAdmin);
+
+		// falls der Benutzer eingelogt ist verändert sich die Navigation
+		if (VaadinSession.getCurrent().getAttribute("login").equals(true)) {
+			if (VaadinSession.getCurrent().getAttribute(User.class)
+					.getAccessLevel() == 2) {// falls der User ein Admin ist
 				nav.setVisible(false);
-				navPublic.setVisible(true);
+				navPublic.setVisible(false);
+				navAdmin.setVisible(true);// Admin-Navigation
+			} else {// ansonsten: Naviagtion für eingeloggte Nutzer
+				nav.setVisible(true);
+				navPublic.setVisible(false);
 				navAdmin.setVisible(false);
 			}
-			
-			//Inhalt hinzufuegen
-			content = new VerticalLayout();
-			content.setMargin(true);
-			content.setWidth("100%");
-			setContent();//Methode zum befuellen des Inhalts aufrufen
-			v.addComponent(content);
-			
-			//Footer hinzufuegen
-			Footer f = new Footer();
-			v.addComponent(f);
-			
-			//rotes Panel unter dem Footer
-			Panel p2 = new Panel();
-			p2.setWidth("100%");
-			p2.addStyleName("red");
-			p2.setHeight("30px");
-			v.addComponent(p2);
-	
+		} else {// ansonsten Public Navigation (für alle)
+			nav.setVisible(false);
+			navPublic.setVisible(true);
+			navAdmin.setVisible(false);
+		}
+
+		// Inhalt hinzufuegen
+		content = new VerticalLayout();
+		content.setMargin(true);
+		content.setWidth("100%");
+		setContent();// Methode zum befuellen des Inhalts aufrufen
+		v.addComponent(content);
+
+		// Footer hinzufuegen
+		Footer f = new Footer();
+		v.addComponent(f);
+
+		// rotes Panel unter dem Footer
+		Panel p2 = new Panel();
+		p2.setWidth("100%");
+		p2.addStyleName("red");
+		p2.setHeight("30px");
+		v.addComponent(p2);
+
 		addComponent(v);
 		this.setExpandRatio(v, 12);
-		
-		//rotes rechtes Panel
+
+		// rotes rechtes Panel
 		Panel p1 = new Panel();
 		p1.setWidth("100%");
 		p1.addStyleName("red");
@@ -118,36 +118,33 @@ public class ErrorPage extends HorizontalLayout implements View{
 		addComponent(p1);
 		this.setExpandRatio(p1, 1);
 	}
-	
+
 	/**
 	 * Sets the Content of the page.
 	 */
-	public void setContent(){
-				
+	public void setContent() {
+
 		// title
-		title = new Label();
-		title.setImmediate(false);
-		title.setWidth("-1px");
-		title.setHeight("-1px");
-		title.setValue("Es ist ein Fehler aufgetreten.");
-		title.addStyleName("title");
-		content.addComponent(title);
-				
-		
-		
+		titleLabel = new Label();
+		titleLabel.setImmediate(false);
+		titleLabel.setWidth("-1px");
+		titleLabel.setHeight("-1px");
+		titleLabel.setValue("Es ist ein Fehler aufgetreten.");
+		titleLabel.addStyleName("title");
+		content.addComponent(titleLabel);
+
 		// text
-		text = new Label();
-		text.setImmediate(false);
-		text.setWidth("-1px");
-		text.setHeight("500px");
-		text.setValue("Das tut uns leid und hätte nicht passieren dürfen!<br/>"
-				+"<br/><br/> Der Fehler kann unter anderem folgende Ursachen haben:"
-				+"<br/><br/> - Sie wollten zu einer nicht verfügbaren Seite navigieren. Beispielsweise können Sie die Profilseite nur aufrufen, wenn Sie eingeloggt sind."
-				+"<br/><br/> - Das Hochladen eines Bildes hat den Fehler verursacht.  Bitte versuchen Sie es später erneut."
-				+"<br/><br/> - Es gab einen Serverfehler. Sollte der Fehler häufiger auftreten, wenden Sie sich bitte an den Administrator.");
-		text.setContentMode(ContentMode.HTML);
-		content.addComponent(text);
+		errorTextLabel = new Label();
+		errorTextLabel.setImmediate(false);
+		errorTextLabel.setWidth("-1px");
+		errorTextLabel.setHeight("500px");
+		errorTextLabel
+				.setValue("Das tut uns leid und hätte nicht passieren dürfen!<br/>"
+						+ "<br/><br/> Der Fehler kann unter anderem folgende Ursachen haben:"
+						+ "<br/><br/> - Sie wollten zu einer nicht verfügbaren Seite navigieren. Beispielsweise können Sie die Profilseite nur aufrufen, wenn Sie eingeloggt sind."
+						+ "<br/><br/> - Das Hochladen eines Bildes hat den Fehler verursacht.  Bitte versuchen Sie es später erneut."
+						+ "<br/><br/> - Es gab einen Serverfehler. Sollte der Fehler häufiger auftreten, wenden Sie sich bitte an den Administrator.");
+		errorTextLabel.setContentMode(ContentMode.HTML);
+		content.addComponent(errorTextLabel);
 	}
-	
-	
 }
