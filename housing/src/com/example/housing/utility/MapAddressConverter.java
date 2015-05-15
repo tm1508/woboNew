63fa27367989a92abdd864621eebb1d37640436c
Julia <Julia@172.16.50.97>
2015-05-15 11:58:12 +0200

    Refactoring

--- housing/src/com/example/housing/utility/MapAddressConverter.java
+++ housing/src/com/example/housing/utility/MapAddressConverter.java
@@ -1,10 +1,8 @@
 package com.example.housing.utility;
 
 import java.io.BufferedReader;
-import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.StringReader;
 import java.io.StringWriter;
 import java.net.HttpURLConnection;
 import java.net.URL;
@@ -18,18 +16,14 @@ import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.transform.OutputKeys;
 import javax.xml.transform.Transformer;
-import javax.xml.transform.TransformerConfigurationException;
 import javax.xml.transform.TransformerException;
 import javax.xml.transform.TransformerFactory;
 import javax.xml.transform.dom.DOMSource;
 import javax.xml.transform.stream.StreamResult;
 
 import org.w3c.dom.Document;
-import org.w3c.dom.Element;
-import org.w3c.dom.NodeList;
 import org.xml.sax.InputSource;
 import org.xml.sax.SAXException;
-import org.xml.sax.XMLReader;
 
 import com.vaadin.tapio.googlemaps.client.LatLon;
 
