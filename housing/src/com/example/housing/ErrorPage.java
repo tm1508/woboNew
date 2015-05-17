d065962604562da7b97185f2aace852180a7b3ce
tm1508 <timo_metzinger@web.de>
2015-05-18 00:01:17 +0200

    Refactor ErrorPage

--- housing/src/com/example/housing/ErrorPage.java
+++ housing/src/com/example/housing/ErrorPage.java
@@ -41,13 +41,15 @@ public class ErrorPage extends CustomHorizontalLayout implements View {
 		errorTextLabel.setWidth("-1px");
 		errorTextLabel.setHeight("500px");
 		errorTextLabel
-				.setValue("Das tut uns leid und hätte nicht passieren dürfen!<br/>"
+				.setValue("Das tut uns leid und hätte nicht passieren dürfen!"
 						+ "<br/><br/> Der Fehler kann unter anderem folgende Ursachen haben:"
-						+ "<br/><br/> -   Sie wollten zu einer nicht verfügbaren Seite navigieren. Beispielsweise können Sie die Profilseite nur aufrufen, wenn Sie eingeloggt sind."
-						+ "<br/><br/> -   Das Hochladen eines Bildes hat den Fehler verursacht. Bitte versuchen Sie es später erneut."
-						+ "<br/><br/> -   Es gab einen Serverfehler. Sollte der Fehler häufiger auftreten, wenden Sie sich bitte an den Administrator.");
+						+ "<br/>- Sie wollten zu einer nicht verfügbaren Seite navigieren. Beispielsweise können Sie die Profilseite nur aufrufen, wenn Sie eingeloggt sind."
+						+ "<br/>- Das Hochladen eines Bildes hat den Fehler verursacht. Bitte versuchen Sie es später erneut."
+						+ "<br/>- Es gab einen Serverfehler. Sollte der Fehler häufiger auftreten, wenden Sie sich bitte an den Administrator.");
 		errorTextLabel.setContentMode(ContentMode.HTML);
 		content.addComponent(errorTextLabel);
+		
+		error.getThrowable().printStackTrace();
 		
 		if(((User) VaadinSession.getCurrent().getSession().getAttribute("user")).getAccessLevel()==2) {
 			content.addComponent(new Label());
