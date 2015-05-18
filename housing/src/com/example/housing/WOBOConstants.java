c8e4335a71c0c3aed04de1e8e5ec64612176b429
Julia <Julia@172.16.50.97>
2015-05-18 14:52:34 +0200

    Konstanten

---
+++ housing/src/com/example/housing/WOBOConstants.java
@@ -0,0 +1,8 @@
+package com.example.housing;
+
+public class WOBOConstants {
+	
+	public final static String EMailBenutzername = "wohnungsboerse_dh@web.de";
+	public final static String EMailPasswort = "wohnungsboerse";
+
+}
